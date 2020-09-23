d4b932c95d592b5487fcab32e26d7622dfe70bb9
Denys Voloshyn <voloshyn.denys@gmail.com>
2020-09-23 18:24:03 +0300

    update dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -20,21 +20,36 @@ const Message = ({message}) => {
 }
 
 const Dialogs = () => {
+
+  const dialogs = [
+    { id: 1, name: "Denys" },
+    { id: 2, name: "Yana" },
+    { id: 3, name: "Iryna" }
+  ]
+
+  const messages = [
+    { id: 1, message: "Hi!" },
+    { id: 2, message: "How are you?" },
+    { id: 3, message: "Everything is alright. Thanks!" }
+  ]
+
+  const dialogsElements = dialogs
+    .map(({id, name}) => <DialogItem key={id} {...{name, id}} />)
+  
+  const messagesElements = messages
+    .map(({id, message}) => <Message key={id} {...{message}} />)
+
   return (
     <section>
       <h1>Dialogs</h1>
       <div className={s.container}>
         <div className={s.dialogs}>
           <ul className={s.dialogs_list}>
-            <DialogItem name="Denys" id="1" />
-            <DialogItem name="Yana" id="2" />
-            <DialogItem name="Iryna" id="3" />
+            { dialogsElements }
           </ul>
         </div>
         <div className={s.messages}>
-          <Message message="Hi!" />
-          <Message message="How are you?" />
-          <Message message="Everything is alright. Thanks!" />
+          { messagesElements }
         </div>
       </div>
     </section>
